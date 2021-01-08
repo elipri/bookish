e9cb4dbd5f497ea6f3a00d942426e8fa85d51109
Elipri <elina.prints@gmail.com>
2021-01-08 19:07:05 +0200

    Minor fixes: auth

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,45 +1,17 @@
 const authService =  require('../services/authService');
 const authController = {};
 
-/* authController.login = async (req,res) => {
-    const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
-    const password = typeof(req.body.password) === 'string' && req.body.password.trim().length > 2 ? req.body.password : false;
-
-    if(email, password) {
-        const loggedIn = await authService.login(email, password);
-        //if login success
-        if (loggedIn) {
-            res.status(200).json({
-                success: true,
-                message: 'Hello, you are logged in'
-            });
-        } else {
-            //login not successful
-            res.status(401).json({
-                success: false,
-                message: 'Check your email, password'
-            });
-        }
-        
-    } else {
-        //error message
-        res.status(400).json({
-            success: false,
-            message: 'Required field(s) missing or invalid'
-        });
-    }
-} */
-
+// Login
+// POST
+// Required: email, pw
+// Optional: none
+// Return: token
 authController.login = async (req, res) => {
     const email = typeof(req.body.email) === 'string' && req.body.email.trim().length > 0 ? req.body.email : false;
     const password = typeof(req.body.password) === 'string' && req.body.password.trim().length > 2 ? req.body.password : false;
     if (email, password) {
-        console.log('jõudis siia');
-        //const loggedIn = await authService.login(email, password);
         const token = await authService.login(email, password);
         if (token) {
-            // Generate token
-
             // Return token for valid user
             res.status(200).json({
                 success: true,
--- api/services/authService.js
+++ api/services/authService.js
@@ -4,24 +4,9 @@ const jwt = require('jsonwebtoken');
 const config = require('../../config');
 const authService = {};
 
-//req email, pw; return true/false
-/* authService.login = async (email, password) => {
-    const user = usersService.readByEmail(email);
-    if(user) {
-        const match = hashService.compare(password, user.password);
-        if (match) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
-} */
 
 authService.login = async (email, password) => {
     const user = await usersService.readByEmail(email);
-    //console.log('authserv:'+user);
     if (user) {
         const match = await hashService.compare(password, user.password);
         if (match) {
@@ -30,7 +15,7 @@ authService.login = async (email, password) => {
                 //{ email: user.email },
                 { id: user.id },
                 config.jwtSecret,
-                { expiresIn: 60 * 60 * 5 } // When will the token expire? In 60*60 seconds: 1h
+                { expiresIn: 60 * 60 * 24 } // When will the token expire? In 60*60 seconds: 1h
             );
             //console.log(token); // Jwt.io - debugger: to check the contents
             return token;
